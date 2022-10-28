4019f808bc3157b3cef8abbd170b14c7ee41be38
alizeebonnefous <alizeebonnefous@localhost>
2022-10-28 15:23:42 +0200

    achevement des questions jusqu'à la question 10. Soit initialisation des variables et affichage de l'étiquette de la bière 1 et la bière 2 ajout d'un constructeur Rajout de 3 autres bières affichage de l'etat de la bière avec la  méthode toString() rajouot de quelques autre biere et affichage de leur etat

--- TP2_Bieres_BONNEFOUS/src/tp2_bieres_bonnefous/BouteilleBiere.java
+++ TP2_Bieres_BONNEFOUS/src/tp2_bieres_bonnefous/BouteilleBiere.java
@@ -9,22 +9,43 @@ package tp2_bieres_bonnefous;
  * @author alizeebonnefous
  */
 public class BouteilleBiere {  //récipient, plat, moule
+
     String nom;
     double degreAlcool;
     String brasserie;
     boolean ouverte;
-    
+
     public void lireEtiquette() {
-System.out.println("Bouteille de " + nom +" (" + degreAlcool + " degres) \nBrasserie : " + brasserie ) ;
+        System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degres) \nBrasserie : " + brasserie);
 
+    }
 
-}
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+    }
 
- public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
-   nom = unNom;
-   degreAlcool = unDegre;
-   brasserie = uneBrasserie;
-   ouverte = false;
-}
+    public boolean Décapsuler() {
+        if (ouverte == false) {               //== : comparaison (si... =... alors)
+            ouverte = true;                     //= : egalité (... prend la valeur...)
+            return true;
+        } else {
+            System.out.println("erreur : biere déjà ouverte");
+            return false;
+        }
+    }
 
+    @Override
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degrés) Ouverte ? ";
+        if (ouverte == true) {
+            chaine_a_retourner += "oui";
+        } else {
+            chaine_a_retourner += "non";
+        }
+        return chaine_a_retourner;
+    }
 }
