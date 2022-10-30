fd1f6998ad7166a949a2d4f0e8c8510dacd96da7
alizeebonnefous <alizeebonnefous@localhost>
2022-10-30 23:02:45 +0100

    1. Création d'une première classe et quelques objets associés achevement des questions jusqu'à la question 10. Soit initialisation des variables et affichage de l'étiquette de la bière 1 et la bière 2 ajout d'un constructeur Rajout de 3 autres bières affichage de l'etat de la bière avec la  méthode toString() rajout de quelques autre biere et affichage de leur etat
    2. Un convertisseur de températures : version classes
    création du nouveau projet
    ajout de la classe convertisseur
    classe de convertisseur complétée (attributs + methodes signifiées)
    création de 2 nouveau objet Convert1 et Convert2
    affichage du nombre de conversions pour chaque objet à l'aide d'un scanner
    afficahge d'un menu + permission du choix de la conversion et de la valeur associé par l'utilisateur
    
    3. Manipulation des objets par leur ref

---
+++ TP2_manip_BONNEFOUS/src/tp2_manip_bonnefous/TP2_manip_BONNEFOUS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_bonnefous;
+
+/**
+ *
+ * @author alizeebonnefous
+ */
+public class TP2_manip_BONNEFOUS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        Tartiflette assiette3 = assiette2 ;
+        assiette2 = assiette1 ;
+        assiette1 = assiette3 ;
+        System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories);
+        System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories);
+       
+    }
+    
+}
+    
